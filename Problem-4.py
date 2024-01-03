84114db0607617db249611f9d2ffcd5c6f555cd8
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-01-03 19:13:51 +0530

    Leap Year using Functions

--- Problem-4.py
+++ Problem-4.py
@@ -3,7 +3,6 @@
 2. The year can be evenly divided by 100, it is NOT a leap year, unless:
 3. The year is also evenly divisible by 400. Then it is a leap year.
 '''
-
 def is_leap(year):
     leap = False
 
