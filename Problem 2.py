a2fc6c325cfa27706edbb0bbe4d7f9c7a24aba3e
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-01-03 18:52:05 +0530

    Arithmetic Operator

--- Problem 2.py
+++ Problem 2.py
@@ -3,10 +3,16 @@ The provided  stub reads two integers from STDIN a, and b.
 Output:
 a+b
 a-b
+a*b
 a/b
+a//b
 '''
 a=int(input())
 b=int(input())
 print(a+b)
 print(a-b)
 print(a*b)
+print(a/b)
+print(a//b)
+
+
