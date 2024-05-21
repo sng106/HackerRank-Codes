60635a11afb01c3652cfb87b6216ed1ef5a35068
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-05-21 19:10:49 +0530

    .

---
+++ 14. Sentence Creation.py
@@ -0,0 +1,18 @@
+'''
+Sample Input:
+Ross
+Taylor
+
+Sample Output 0
+Hello Ross Taylor! You just delved into python.
+'''
+Code:
+
+
+def print_full_name(first, last):
+    print (f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
