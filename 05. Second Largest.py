597f0715935e65c704dd316a5156dda09785a836
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-05-21 13:34:39 +0530

    .

--- 05. Second Largest.py
+++ 05. Second Largest.py
@@ -1,5 +1,14 @@
+'''Finding the Second Largest Number in a array/list
+Input:
+5
+64 78 54 23 90
+
+Output:
+78
+'''
+#Code:
 def ur(array):
-  unique=sorted(set(array), reverse=True)
+  unique=sorted(set(array), reverse=True) 
   if len(unique) >= 2:
       return unique[1]
   else:
